2b904fbad3724a6bb35eba2e06ef13181129ae57
sudarshansw7 <142533426+sudarshansw7@users.noreply.github.com>
2025-07-24 12:38:59 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y openjdk-17-jdk
+java --version
+sleep 5
 sudo apt-get install -y git
+git --version
+sleep 5
 sudo apt-get install -y maven
+mvn --version
+sleep 5
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
